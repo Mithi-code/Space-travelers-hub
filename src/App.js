3e5f2b66301c26e169beaa3a7b55c5fe5020e7a5
youmari <youmari300@gmail.com>
2022-01-25 16:42:13 +0100

    connect redux store to app.js and display the data in missions.js in a table

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       <Routes>
         <Route exact element={<Missions />} path="/missions" />
       </Routes>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world</h1>
     </div>
   );
 }
