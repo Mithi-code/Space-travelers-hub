432809dff8593f4d7b5432b28b755b4898a8aa81
mithlesh kumar <mithlesh.9m@gmail.com>
2022-01-25 18:45:05 +0530

    rocket-setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router';
 import Header from './Components/Header';
 import Missions from './Components/Missions';
+import Rockets from './Components/Rockets';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Routes>
-        <Route exact element={<Missions />} path="/missions" />
+        <Route exact element={<Rockets />} path='/rockets' />
+        <Route exact element={<Missions />} path='/missions' />
       </Routes>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world</h1>
+      <h1 className='text-3xl font-bold underline text-red-500'>Hello world</h1>
     </div>
   );
 }
