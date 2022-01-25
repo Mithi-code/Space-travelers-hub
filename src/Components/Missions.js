ec555514137aae7e2b0c07d3fa6ad04198ec9682
youmari <youmari300@gmail.com>
2022-01-25 16:40:15 +0100

    create redux actions creators fro fetching missions data and the reducer

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -1,7 +1,50 @@
-const Missions = () => (
-  <div>
-    <p>Missions are here</p>
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissionsData } from '../Redux/Missions/Missions';
+
+const Missions = () => {
+  const data = useSelector((state) => state.missionsReducer.missions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissionsData());
+  }, []);
+
+  return (
+    <table className="table-fixed m-20 border-solid ring ring-gray-300">
+      <thead>
+        <tr>
+          <th>Missions</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map((item) => (
+          <tr
+            className=" border-solid border-2 border-gray-200"
+            key={item.mission_id}
+          >
+            <td className=" border-solid border-2 border-gray-200 text-center">
+              {item.mission_name}
+            </td>
+            <td className=" border-solid border-2 border-gray-200 p-6">
+              {item.description}
+            </td>
+            <td className=" border-solid border-2 border-gray-200">
+              <button className=" w-32 bg-gray-500 text-white font-bold m-3 rounded" type="button">
+                NOT A MEMBER
+              </button>
+            </td>
+            <td>
+              <button className=" w-32 p-2 border-solid border-2 border-black m-3 rounded" type="button">
+                join Mission
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Missions;
