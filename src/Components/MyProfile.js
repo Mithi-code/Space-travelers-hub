fcd4fafdf60c799100396d0e465891f81f9c6c17
youmari <youmari300@gmail.com>
2022-01-25 23:42:39 +0100

    add style to my mission

--- src/Components/MyProfile.js
+++ src/Components/MyProfile.js
@@ -5,18 +5,20 @@ const MyProfile = () => {
   const joinedMissions = missions.filter(((mission) => mission.reserved));
   return (
     <div className="flex-row">
-      <div>
-        <h3 className="text-lg">Missions</h3>
-        <ul className="flex-col border-2 border-black m-1  w-64">
-          {joinedMissions?.map((mission) => (
-            <li
-              className="p-4 border-black border divide-x-0"
-              key={mission.mission_id}
-            >
-              {mission.mission_name}
-            </li>
-          ))}
-        </ul>
+      <div className="m-4">
+        <h3 className="text-xl font-bold">My Missions</h3>
+        {joinedMissions.length ? (
+          <ul className="flex-col border-2 border-black m-1 w-64">
+            {joinedMissions?.map((mission) => (
+              <li
+                className="p-4 border-black border divide-x-0"
+                key={mission.mission_id}
+              >
+                {mission.mission_name}
+              </li>
+            ))}
+          </ul>
+        ) : ''}
       </div>
     </div>
   );
