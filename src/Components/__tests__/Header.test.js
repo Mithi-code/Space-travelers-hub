b819fa21339d67f671d060e98914b76c4c67b5cd
youmari <youmari300@gmail.com>
2022-01-26 21:28:53 +0100

    Refactor the code

--- src/Components/__tests__/Header.test.js
+++ src/Components/__tests__/Header.test.js
@@ -1,15 +1,17 @@
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { Provider } from "react-redux";
-import store from "../../Redux/store/store";
-import Header from "../Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Rockets from "../Rockets";
-import Missions from "../Missions";
-import MyProfile from "../MyProfile";
+import {
+  render, fireEvent, screen, waitFor,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import store from '../../Redux/store/store';
+import Header from '../Header';
+import Rockets from '../Rockets';
+import Missions from '../Missions';
+import MyProfile from '../MyProfile';
 
-describe("Header checking...", () => {
-  test("should render and go to mission page", async () => {
+describe('Header checking...', () => {
+  test('should render and go to mission page', async () => {
     render(
       <Provider store={store}>
         <BrowserRouter>
@@ -20,29 +22,29 @@ describe("Header checking...", () => {
             <Route exact element={<MyProfile />} path="/myprofile" />
           </Routes>
         </BrowserRouter>
-      </Provider>
+      </Provider>,
     );
-    const missionsLinkBtn = screen.getByRole("link", { name: /missions/i });
-    fireEvent.click(missionsLinkBtn)
-    const missionName = await waitFor(() => screen.getByText("Thaicom"));
+    const missionsLinkBtn = screen.getByRole('link', { name: /missions/i });
+    fireEvent.click(missionsLinkBtn);
+    const missionName = await waitFor(() => screen.getByText('Thaicom'));
     expect(missionName).toBeInTheDocument();
   });
-   test("should render and go to Myprofile page", () => {
-     render(
-       <Provider store={store}>
-         <BrowserRouter>
-           <Header />
-           <Routes>
-             <Route exact element={<Rockets />} path="/" />
-             <Route exact element={<Missions />} path="/missions" />
-             <Route exact element={<MyProfile />} path="/myprofile" />
-           </Routes>
-         </BrowserRouter>
-       </Provider>
-     );
-     const myProfileBtn = screen.getByRole("link", { name: /my profile/i });
-     fireEvent.click(myProfileBtn);
-     const profileHeading = screen.getByRole("heading", { name: /my missions/i });
-     expect(profileHeading).toBeInTheDocument();
-   });
-})
+  test('should render and go to Myprofile page', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact element={<Rockets />} path="/" />
+            <Route exact element={<Missions />} path="/missions" />
+            <Route exact element={<MyProfile />} path="/myprofile" />
+          </Routes>
+        </BrowserRouter>
+      </Provider>,
+    );
+    const myProfileBtn = screen.getByRole('link', { name: /my profile/i });
+    fireEvent.click(myProfileBtn);
+    const profileHeading = screen.getByRole('heading', { name: /my missions/i });
+    expect(profileHeading).toBeInTheDocument();
+  });
+});
--- src/Components/__tests__/MyProfile.test.js
+++ src/Components/__tests__/MyProfile.test.js
@@ -1,15 +1,17 @@
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { Provider } from "react-redux";
-import store from "../../Redux/store/store";
-import Header from "../Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Rockets from "../Rockets";
-import Missions from "../Missions";
-import MyProfile from "../MyProfile";
+import {
+  render, fireEvent, screen, waitFor,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import store from '../../Redux/store/store';
+import Header from '../Header';
+import Rockets from '../Rockets';
+import Missions from '../Missions';
+import MyProfile from '../MyProfile';
 
-describe("Myproifle  checking...", () => {
-  test("should render and add Thaicom mission to myprofile page ", async () => {
+describe('Myproifle  checking...', () => {
+  test('should render and add Thaicom mission to myprofile page ', async () => {
     render(
       <Provider store={store}>
         <BrowserRouter>
@@ -20,15 +22,15 @@ describe("Myproifle  checking...", () => {
             <Route exact element={<MyProfile />} path="/myprofile" />
           </Routes>
         </BrowserRouter>
-      </Provider>
+      </Provider>,
     );
-    const missionsLinkBtn = screen.getByRole("link", { name: /missions/i });
+    const missionsLinkBtn = screen.getByRole('link', { name: /missions/i });
     fireEvent.click(missionsLinkBtn);
-    const JoinmissionBtn = await waitFor(() => screen.findByTestId("9D1B7E0"));
-    fireEvent.click(JoinmissionBtn)
-    const myProfileLinkBtn = screen.getByRole("link", { name: /missions/i });
-    fireEvent.click(myProfileLinkBtn)
-    const missionName = screen.getByText("Thaicom");
+    const JoinmissionBtn = await waitFor(() => screen.findByTestId('9D1B7E0'));
+    fireEvent.click(JoinmissionBtn);
+    const myProfileLinkBtn = screen.getByRole('link', { name: /missions/i });
+    fireEvent.click(myProfileLinkBtn);
+    const missionName = screen.getByText('Thaicom');
     expect(missionName).toBeInTheDocument();
   });
 });
