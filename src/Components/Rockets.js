fa9a82c0cd2276c19adccc8ca5a7031346e4bdfd
mithlesh kumar <mithlesh.9m@gmail.com>
2022-01-26 10:34:25 +0530

    stylelint error

--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -20,7 +20,7 @@ const Rockets = () => {
             <h1>{item.rocket_name}</h1>
             <p>{item.description}</p>
             <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              reserve
+              Reserve Rocket
             </button>
           </div>
         </div>
